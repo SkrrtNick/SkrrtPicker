e5853b252685bbd03de43973776ecfa60a61d1c1
SkrrtNick <79165336+SkrrtNick@users.noreply.github.com>
2021-08-15 19:30:20 +0100

    Initial Commit

--- src/scripts/tasks/Picker.java
+++ src/scripts/tasks/Picker.java
@@ -4,15 +4,13 @@ import dax.walker_engine.interaction_handling.NPCInteraction;
 import org.tribot.api.General;
 import org.tribot.api2007.Interfaces;
 import org.tribot.api2007.WorldHopper;
-import org.tribot.api2007.types.RSArea;
-import org.tribot.api2007.types.RSGroundItem;
-import org.tribot.api2007.types.RSObject;
-import scripts.data.Vars;
+import org.tribot.api2007.types.*;
 import scripts.api.task.Priority;
 import scripts.api.task.Task;
 import scripts.api.util.antiban.Antiban;
 import scripts.api.util.functions.*;
 import scripts.api.util.numbers.Reactions;
+import scripts.data.Vars;
 
 import static scripts.data.Vars.*;
 
@@ -37,12 +35,12 @@ public class Picker implements Task {
     @Override
     public void execute() {
         Antiban.activateRun();
-        if(initItemCount == 0 && pickupItemID == 0){
-            if(Inventory07.getAll().length > 0){
+        if (initItemCount == 0 && pickupItemID == 0) {
+            if (Inventory07.getAll().length > 0) {
                 pickupItemID = Inventory07.getAll()[0].getID();
             }
         }
-        if(Inventory07.isFull()){
+        if (Inventory07.isFull()) {
             shouldBank = true;
         }
         if (Player07.distanceTo(Vars.customTile) > 30) {
@@ -53,23 +51,27 @@ public class Picker implements Task {
             if (Vars.groundItemObject) {
                 RSGroundItem[] items = Interaction.getGroundItems(Vars.runtimeSettings.getPickupItemName());
                 if (items.length > 0) {
-                    String[] actions = items[0].getDefinition().getGroundActions();
-                    if (!items[0].isClickable()) {
-                        items[0].adjustCameraTo();
+                    RSItemDefinition itemDefinition = items[0].getDefinition();
+                    if (itemDefinition != null) {
+                        String[] actions = itemDefinition.getGroundActions();
+                        if (!items[0].isClickable()) {
+                            items[0].adjustCameraTo();
+                        }
+                        if (items[0].click(actions[0])) {
+                            General.sleep(Reactions.getNormal());
+                            Sleep.until(() -> inventCount < Inventory07.getAll().length);
+                            if (Interfaces.isInterfaceSubstantiated(553)) {
+                                Interfaces.closeAll();
+                            }
+                            if (NPCInteraction.isConversationWindowUp()) {
+                                Interaction.handleContinue();
+                            }
+                            if (pickupItemID == 0 && Inventory07.getAll().length == 1) {
+                                pickupItemID = Inventory07.getAll()[0].getID();
+                            }
+                        }
                     }
-                    if (items[0].click(actions[0])) {
-                        General.sleep(Reactions.getNormal());
-                        Sleep.until(() -> inventCount < Inventory07.getAll().length);
-                        if(Interfaces.isInterfaceSubstantiated(553)) {
-                            Interfaces.closeAll();
-                        }
-                        if(NPCInteraction.isConversationWindowUp()){
-                            Interaction.handleContinue();
-                        }
-                        if (pickupItemID == 0 && Inventory07.getAll().length == 1) {
-                            pickupItemID = Inventory07.getAll()[0].getID();
-                        }
-                    }
+
                 } else {
                     attempt++;
                     General.sleep(Reactions.getNormal());
@@ -91,40 +93,43 @@ public class Picker implements Task {
                 //TODO interactable object handling
                 RSObject[] items = Interaction.getObjects(Vars.runtimeSettings.getPickupItemName());
                 if (items.length > 0) {
-                    String[] actions = items[0].getDefinition().getActions();
-                    if (!items[0].isClickable()) {
-                        items[0].adjustCameraTo();
-                    }
-                    if (items[0].click(actions[0])) {
-                        General.sleep(Reactions.getNormal());
-                        Sleep.until(() -> inventCount < Inventory07.getAll().length);
-                        if(Interfaces.isInterfaceSubstantiated(553)) {
-                            Interfaces.closeAll();
+                    RSObjectDefinition objectDefinition = items[0].getDefinition();
+                    if (objectDefinition != null) {
+                        String[] actions = objectDefinition.getActions();
+                        if (!items[0].isClickable()) {
+                            items[0].adjustCameraTo();
                         }
-                        if(NPCInteraction.isConversationWindowUp()){
-                            Interaction.handleContinue();
-                        }
-                        if (pickupItemID == 0 && Inventory07.getAll().length == 1) {
-                            pickupItemID = Inventory07.getAll()[0].getID();
-                        }
-                    } else {
-                        attempt++;
-                        General.sleep(Reactions.getNormal());
-                        if (Vars.runtimeSettings.isShouldWorldHop() && attempt > 5) {
-                            if (Vars.worldHopToP2P) {
-                                Logging.message("WorldHopper Event", "Hopping to Random P2P world");
-                                WorldHopper.changeWorld(WorldHopper.getRandomWorld(true, false));
-                            } else {
-                                Logging.message("WorldHopper Event", "Hopping to Random F2P world");
-                                WorldHopper.changeWorld(WorldHopper.getRandomWorld(false, false));
+                        if (items[0].click(actions[0])) {
+                            General.sleep(Reactions.getNormal());
+                            Sleep.until(() -> inventCount < Inventory07.getAll().length);
+                            if (Interfaces.isInterfaceSubstantiated(553)) {
+                                Interfaces.closeAll();
+                            }
+                            if (NPCInteraction.isConversationWindowUp()) {
+                                Interaction.handleContinue();
+                            }
+                            if (pickupItemID == 0 && Inventory07.getAll().length == 1) {
+                                pickupItemID = Inventory07.getAll()[0].getID();
+                            }
+                        } else {
+                            attempt++;
+                            General.sleep(Reactions.getNormal());
+                            if (Vars.runtimeSettings.isShouldWorldHop() && attempt > 5) {
+                                if (Vars.worldHopToP2P) {
+                                    Logging.message("WorldHopper Event", "Hopping to Random P2P world");
+                                    WorldHopper.changeWorld(WorldHopper.getRandomWorld(true, false));
+                                } else {
+                                    Logging.message("WorldHopper Event", "Hopping to Random F2P world");
+                                    WorldHopper.changeWorld(WorldHopper.getRandomWorld(false, false));
+                                }
+                            }
+                            if (!Vars.customArea.contains(Player07.getPosition())) {
+                                Traversing.walkTo(Vars.customArea);
                             }
                         }
-                        if (!Vars.customArea.contains(Player07.getPosition())) {
-                            Traversing.walkTo(Vars.customArea);
-                        }
                     }
-                }
 
+                }
             }
         }
     }
